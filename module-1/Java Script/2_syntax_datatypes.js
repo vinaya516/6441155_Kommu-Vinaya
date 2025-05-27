f83f4f0cbb3d64163ac29d5a7745d4e881f1cd26
vinaya516 <vinayakommu@gmail.com>
2025-05-27 14:39:00 +0530

    Add files via upload

---
+++ module-1/Java Script/2_syntax_datatypes.js
@@ -0,0 +1,9 @@
+// Syntax, Data Types, and Operators
+const eventName = "City Music Fest";
+const eventDate = "2025-07-10";
+let seats = 100;
+
+console.log(`Event: ${eventName} | Date: ${eventDate} | Seats Available: ${seats}`);
+
+seats--;
+console.log(`Seats left after registration: ${seats}`);
