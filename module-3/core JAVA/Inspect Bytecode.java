f8e3891faaa667629f97d27e10da4fb4775b4bf5
vinaya516 <vinayakommu@gmail.com>
2025-05-27 16:03:37 +0530

    Delete module-3 directory

--- module-3/core JAVA/Inspect Bytecode.java
+++
@@ -1,8 +0,0 @@
-public class BytecodeExample {
-    public static void greet() {
-        System.out.println("Hello Bytecode!");
-    }
-    public static void main(String[] args) {
-        greet();
-    }
-}
--- module-3/core JAVA/OperatorPrecedence.java
+++
@@ -1,7 +0,0 @@
-public class OperatorPrecedence {
-    public static void main(String[] args) {
-        int result = 10 + 5 * 2;
-        System.out.println("Result of 10 + 5 * 2 = " + result);
-        // Explanation: 5*2 = 10, then 10+10 = 20
-    }
-}
