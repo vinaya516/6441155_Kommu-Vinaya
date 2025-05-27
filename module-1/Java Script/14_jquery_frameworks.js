4f519a1ed5ab3c22898d3f2ba43b408fb8a29cb6
vinaya516 <vinayakommu@gmail.com>
2025-05-27 14:45:55 +0530

    Add files via upload

---
+++ module-1/Java Script/14_jquery_frameworks.js
@@ -0,0 +1,11 @@
+// jQuery and Frameworks
+$('#registerBtn').click(function () {
+  alert("Registered via jQuery!");
+});
+
+$('.eventCard').fadeIn().fadeOut();
+
+/* Benefit of frameworks like React/Vue:
+   - Reactive state updates
+   - Reusable components
+   - Virtual DOM = faster UI */
